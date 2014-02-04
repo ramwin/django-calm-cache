3a4113c6c0c4643c95e41172f7626a56697f93ea
svitkovskiy <stanislav.vitkovskiy@pitcre.ws>
2014-02-04 13:59:20 +1100

    Added page cache tests

--- tests/myapp/tests/__init__.py
+++ tests/myapp/tests/__init__.py
@@ -1,3 +1,4 @@
 from .calmcache import CalmCacheTest
 from .key_func import KeyFuncTest
 from .memcache import MemcacheZipMixinTest, BinPyLibMCCacheTest
+from .page_cache import PageCacheTest
--- tests/myapp/tests/memcache.py
+++ tests/myapp/tests/memcache.py
@@ -1,4 +1,3 @@
-
 from django.test import TestCase
 from django.test.utils import override_settings
 from django.utils.unittest import skipUnless
