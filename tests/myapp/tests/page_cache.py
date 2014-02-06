03b6a4563a5242aa920f75c22a6714dfd79d17af
svitkovskiy <stanislav.vitkovskiy@pitcre.ws>
2014-02-06 13:59:52 +1100

    Added tests for Last-Modified. Sped up caching tests.

--- tests/myapp/tests/page_cache.py
+++ tests/myapp/tests/page_cache.py
@@ -4,10 +4,12 @@ import time
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.utils.unittest import skipUnless
+from django.utils.http import http_date
 from django.http import HttpResponse
 from django.contrib.auth.models import User, AnonymousUser
 from django.template import Template
 from django.template.response import SimpleTemplateResponse
+from django.core.cache import get_cache
 
 from calm_cache.decorators import PageCacheDecorator
 
@@ -19,10 +21,10 @@ except ImportError:
     StreamingHttpResponse = None
 
 
-def randomView(request, cache_seconds=None):
+def randomView(request, last_modified=None):
     response = HttpResponse(str(random.randint(0,1e6)))
-    if cache_seconds is not None:
-        response['Cache-Control'] = "max-age=%i" % cache_seconds
+    if last_modified is not None:
+        response['Last-Modified'] = http_date(last_modified)
     return response
 
 
@@ -30,11 +32,13 @@ def templateRandomView(request):
     t = Template("%s" % random.randint(0, 1e6))
     return SimpleTemplateResponse(t)
 
+page_cache = PageCacheDecorator(0.3, cache='testcache')
 
 class PageCacheTest(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
+        get_cache('testcache').clear()
 
     def test_default_key_function(self):
         cache = PageCacheDecorator(1, key_prefix='p')
@@ -108,39 +112,48 @@ class PageCacheTest(TestCase):
         self.assertFalse(cache.should_store(StreamingHttpResponse()))
 
     def test_caching_decorator(self):
-        decorator = PageCacheDecorator(3)
-        decorated_view = decorator(randomView)
+        decorated_view = page_cache(randomView)
         # Confirm that the same URL is cached and returns the same content
         # And cache-related headers
-        request1 = self.factory.get('/?r=1')
-        rsp1 = decorated_view(request1)
-        time.sleep(1.1)
-        rsp2 = decorated_view(request1)
+        request = self.factory.get('/%i' % random.randint(1,1e6))
+        rsp1 = decorated_view(request)
+        time.sleep(0.1)
+        # This should be still cached
+        rsp2 = decorated_view(request)
+        time.sleep(0.3)
+        # But this will expire and be refreshed
+        rsp3 = decorated_view(request)
         self.assertEqual(rsp1.content, rsp2.content)
-        # Different URLs are cached under different keys
-        request2 = self.factory.get('/?r=2')
-        self.assertNotEqual(rsp1.content, decorated_view(request2).content)
+        self.assertNotEqual(rsp1.content, rsp3.content)
 
     def test_caching_template_response(self):
         # Perform the same tests for SimpleTemplateResponse
-        decorator = PageCacheDecorator(3)
-        decorated_view = decorator(templateRandomView)
-        request1 = self.factory.get('/?r=3')
-        rsp1 = decorated_view(request1)
+        decorated_view = page_cache(templateRandomView)
+        request = self.factory.get('/%i' % random.randint(1,1e6))
+        rsp1 = decorated_view(request)
         # Shouldn't be rendered yet
         self.assertFalse(hasattr(rsp1, 'content'))
         rsp1.render()
-        time.sleep(1.1)
-        rsp2 = decorated_view(request1)
+        time.sleep(0.1)
+        rsp2 = decorated_view(request)
         # Should be already rendered because fetched from the cache
         self.assertTrue(hasattr(rsp2, 'content'))
-        self.assertEqual(rsp1.content, rsp2.content)
-        request2 = self.factory.get('/?r=4')
-        rsp3 = decorated_view(request2)
-        # Shouldn't be rendered
+        time.sleep(0.3)
+        rsp3 = decorated_view(request)
+        # Shouldn't be rendered yet again
         self.assertFalse(hasattr(rsp3, 'content'))
         rsp3.render()
+        # Compare content
+        self.assertEqual(rsp1.content, rsp2.content)
         self.assertNotEqual(rsp1.content, rsp3.content)
+
+    def test_caching_decorator_different_urls(self):
+        decorated_view = page_cache(randomView)
+        # Different URLs are cached under different keys
+        request1 = self.factory.get('/%i' % random.randint(1,1e6))
+        request2 = self.factory.get('/%i' % random.randint(1,1e6))
+        self.assertNotEqual(decorated_view(request1).content,
+                            decorated_view(request2).content)
 
     def test_uncacheable_requests(self):
         # Test authenticated requests (shouldn't cache)
@@ -155,3 +168,17 @@ class PageCacheTest(TestCase):
         request = self.factory.head('/')
         self.assertNotEqual(decorated_view(request).content,
                             decorated_view(request).content)
+
+    def test_last_modified_default(self):
+        decorated_view = page_cache(randomView)
+        # Last-Modified has to be set to "now"
+        request = self.factory.get('/%i' % random.randint(1,1e6))
+        rsp = decorated_view(request)
+        self.assertEqual(rsp.get('Last-Modified'), http_date())
+
+    def test_last_modified_from_response(self):
+        decorated_view = page_cache(randomView)
+        # Last-Modified has to be set to whatever was in the original response
+        request = self.factory.get('/%i' % random.randint(1,1e6))
+        rsp = decorated_view(request, 123456)
+        self.assertEqual(rsp.get('Last-Modified'), http_date(123456))
