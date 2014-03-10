89d150d2891ab679af72af0978b18b946cf7ab1f
svitkovskiy <stanislav.vitkovskiy@pitcre.ws>
2014-03-11 10:49:59 +1100

    Added setup.py test command

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
+import sys
 from os.path import dirname, join
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCmd
+
+
+class ToxCmd(TestCmd):
+    def finalize_options(self):
+        # Old classes don't understand super()
+        TestCmd.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import tox
+        sys.exit(tox.cmdline(self.test_args))
+
 
 def fread(fn):
     with open(join(dirname(__file__), fn), 'r') as f:
@@ -14,4 +28,6 @@ setup(
     license=fread('LICENSE'),
     packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=False,
+    tests_require=['tox'],
+    cmdclass={'test': ToxCmd},
 )
